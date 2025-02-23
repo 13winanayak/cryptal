f9758e502c438fe2d22273d9f5896872c470ee30
Vinayak <saxenavinayak5@gmail.com>
2025-02-23 12:59:34 +0530

    complete the coin api , implements controller class

--- src/main/java/com/cryptal/trading/service/CoinServiceImp.java
+++ src/main/java/com/cryptal/trading/service/CoinServiceImp.java
@@ -129,17 +129,61 @@ public class CoinServiceImp implements CoinService{
 
 
     @Override
-    public String searchCoin(String keyword) {
-        return "";
+    public String searchCoin(String keyword) throws Exception {
+        String url = "https://api.coingecko.com/api/v3/search?query="+keyword;
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+            return response.getBody();
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
-    public String getTop50CoinsByMarketCapRank() {
-        return "";
+    public String getTop50CoinsByMarketCapRank() throws Exception {
+        String url = "https://api.coingecko.com/api/v3/coins/markets/vs_currency=usd&per_page=50";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+            return response.getBody();
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            throw new Exception(e.getMessage());
+        }
     }
 
     @Override
-    public String GetTreadingCoins() {
-        return "";
+    public String getTreadingCoins() throws Exception {
+        String url = "https://api.coingecko.com/api/v3/search/treading";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+            return response.getBody();
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
