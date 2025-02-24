675726d7c12611caf12a13f5a79b2014e75611c5
Vinayak <saxenavinayak5@gmail.com>
2025-02-24 23:53:03 +0530

    complete Order controlller

--- src/main/java/com/cryptal/trading/service/OrderService.java
+++ src/main/java/com/cryptal/trading/service/OrderService.java
@@ -16,6 +16,6 @@ public interface OrderService {
 
     List<Order> getAllOrdersOfUser(Long userID, OrderType orderType, String assetSymbol);
 
-    Order processOrder(Coin coin, double quantity, OrderType orderType, User user);
+    Order processOrder(Coin coin, double quantity, OrderType orderType, User user) throws Exception;
 
 }
--- src/main/java/com/cryptal/trading/service/OrderServiceImp.java
+++ src/main/java/com/cryptal/trading/service/OrderServiceImp.java
@@ -91,38 +91,54 @@ public class OrderServiceImp implements OrderService {
         return savedOrder;
 
                         }
-    
 
 
-@Transactional
-public Order sellAsset(Coin coin, double quantity, User user) throws Exception {
-    if (quantity <= 0) {
-        throw new Exception("quantity should be >0");
+
+    @Transactional
+    public Order sellAsset(Coin coin, double quantity, User user) throws Exception {
+        if (quantity <= 0) {
+            throw new Exception("quantity should be > 0");
+        }
+
+        double sellPrice = coin.getCurrentPrice();
+
+        double buyPrice = assetToSell.getPrice();
+
+        OrderItem orderItem = createOrderItem(coin, quantity, buyPrice, sellPrice);
+
+        Order order = createOrder(user, orderItem, OrderType.SELL);
+        orderItem.setOrder(order);
+
+        if (assetToSell.getQuantity() >= quantity) {
+            order.setStatus(OrderStatus.SUCCESS);
+            order.setOrderType(OrderType.SELL);
+            Order savedOrder = orderRepository.save(order);
+
+            walletService.payOrderPayment(order, user);
+
+            Asset updatedAsset = assetService.updateAsset(assetToSell.getId(), -quantity);
+            if (updatedAsset.getQuantity() * coin.getCurrentPrice() <= 1) {
+                assetService.deleteAsset(updatedAsset.getId());
+            }
+            return savedOrder;
+        }
+
+        throw new Exception("Insufficient quantity to sell");
+
     }
-
-    double buyPrice = coin.getCurrentPrice();
-
-    OrderItem orderItem = createOrderItem(coin, quantity, buyPrice, 0);
-
-    Order order = createOrder(user, orderItem, OrderType.BUY);
-    orderItem.setOrder(order);
-
-    walletService.payOrderPayment(order, user);
-
-    order.setStatus(OrderStatus.SUCCESS);
-    order.setOrderType(OrderType.BUY);
-    Order savedOrder = orderRepository.save(order);
-
-// create asset
-
-    return savedOrder;
-
-}
 
 
 
     @Override
-    public Order processOrder(Coin coin, double quantity, OrderType orderType, User user) {
-        return null;
+    @Transactional
+    public Order processOrder(Coin coin, double quantity, OrderType orderType, User user) throws Exception {
+        if (orderType.equals(OrderType.BUY)) {
+            return buyAsset(coin, quantity, user);
+        }
+        else if (orderType.equals(OrderType.SELL)) {
+            return sellAsset(coin, quantity, user);
+        }
+        throw new Exception("invalid order type");
+
     }
 }
